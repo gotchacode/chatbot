624962d717eb1a25bc434da4f6136baee5d55cab
vinit Kumar <vinit.kumar@changer.nl>
2014-08-10 15:08:19 +0530

    add basic structure

---
+++ src/chatbot.c
@@ -0,0 +1,16 @@
+// chatbot.c
+//
+// Copyright (c) 2014 Vinit Kumar <vinit1414.08@bitmesra.ac.in>
+//
+//
+
+
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+
+  while(1) {
+    //logic that runts the bot
+  }
+}
